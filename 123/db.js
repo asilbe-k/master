1f3ad7be2aed768ea740fd3b88c7a7c87b856942
SUKUNA <asilbekoralboyev@gmail.com>
2025-01-25 14:41:21 +0500

    master
    master

---
+++ 123/db.js
@@ -0,0 +1,91 @@
+let products = [
+    {
+        id: 1, 
+        nomi: "Iphone 16 pro max",
+        narxi: 1100 , 
+        brend: "apple", 
+        rasmi:["https://www.gottabemobile.com/wp-content/uploads/2014/07/iPhone-Render-2.jpg"],
+    },
+    {
+        id: 2, 
+        nomi: "Iphone 15 pro max",
+        narxi: 1000 , 
+        brend: "apple", 
+        rasmi:["https://i.ytimg.com/vi/uTWE1BOIDkg/maxresdefault.jpg"],
+    },
+    {
+        id: 3, 
+        nomi: "Iphone 14 pro max ",
+        narxi: 990 , 
+        brend: "apple", 
+        rasmi:["https://avatars.mds.yandex.net/i?id=295a6035d2f9e0fef9f2d4bbdff15d3c_l-4576071-images-thumbs&n=13"]
+    },
+    {
+        id: 4, 
+        nomi: "Iphone 12 pro",
+        narxi: 970 , 
+        brend: "apple", 
+        rasmi:["https://ap71.ru/wp-content/uploads/2021/02/38573-73428-iPhone-12-Pro-Blue-Angle-xl.jpg"]
+    },
+
+    {
+        id: 5, 
+        nomi: "Iphone 12",
+        narxi: 920 , 
+        brend: "apple", 
+        rasmi:["https://m-cdn.phonearena.com/images/articles/367426-image/P1640294-Edit.jpg"]
+    },
+    {
+        id: 6, 
+        nomi: "Iphone 15 pro ",
+        narxi: 780 , 
+        brend: "apple", 
+        rasmi:["https://m-cdn.phonearena.com/images/articles/367426-image/P1640294-Edit.jpg"]
+    },
+    {
+        id: 7, 
+        nomi: "Iphone 13 pro  ",
+        narxi:780 , 
+        brend: "apple", 
+        rasmi:["https://m-cdn.phonearena.com/images/articles/367426-image/P1640294-Edit.jpg"]
+    },
+    {
+        id: 8, 
+        nomi: "Iphone 12 pro",
+        narxi: 980 , 
+        brend: "apple", 
+        rasmi:["https://isales-spb.ru/media/232/23263.jpeg"]
+    },
+
+    {
+        id: 9, 
+        nomi: "Iphone 12 pro ",
+        narxi: 850 , 
+        brend: "apple", 
+        rasmi:["https://avatars.mds.yandex.net/i?id=f6109c692dc3de60d2ba6e477f2280e4_l-9137964-images-thumbs&ref=rim&n=13&w=800&h=700"]
+    },
+    {
+        id: 10, 
+        nomi: "Iphone 14 plus",
+        narxi: 830 , 
+        brend: "apple", 
+        rasmi:["https://avatars.mds.yandex.net/i?id=97de50e346535e4b7d5de9f9d2769e6a_l-4027983-images-thumbs&ref=rim&n=13&w=1600&h=1400"]
+    },
+    {
+        id: 11, 
+        nomi: "Iphone 13 plus ",
+        narxi: 560 , 
+        brend: "apple", 
+        rasmi:["https://ipac31.ru/image/cache/data/product/iPhone/iPhone%20SE%202022/iphone-se-starlight-2022-2-700x700-700x700.jpg"]
+    },
+    {
+        id: 12, 
+        nomi: "Iphone 14 pro",
+        narxi: 780 , 
+        brend: "apple", 
+        rasmi:["https://imobile77.ru/wa-data/public/shop/products/59/05/10559/images/39636/39636.970.jpg"]
+    },
+ 
+]
+
+
